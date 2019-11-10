fe4479bd0d3ee80abd6b4b9a0350e22b239f4fb3
Tim McMaster <tim@mcmaster.id.au>
2019-11-10 17:40:40 +1100

    fixed the UI blocking issue.

--- index.js
+++ index.js
@@ -1,22 +1,70 @@
-import {test, isPrime, isSquare, isFibonacci} from "./utils.js";
+import {isFibonacci, isPrime, isSquare, test} from "./utils.js";
 
-const divisorMax = 3;
-const trainingSize = 300;
-const testSize = 50;
-const trainingCycles = 200;
+const origLog = console.log;
+console.log = (message) => {
+    const div = document.getElementById('results');
+    const ul = div.getElementsByTagName('ul')[0];
+    const li = document.createElement('li');
+    li.textContent = message;
+    ul.appendChild(li);
+    origLog(message);
+};
 
-console.log(`Training Size: ${trainingSize}, TestSize: ${testSize}, Training Cycles: ${trainingCycles}`);
+window.onload = () => {
+    document.getElementById('run').addEventListener('click', () => {
+        // worker.postMessage({message: 'HI THERE'})
+        clear();
+        let counter = 0;
+        let message = undefined;
+        let generator = run();
+        const runTest = () => {
+            if (message !== undefined) console.log(message);
+            counter++;
+            setTimeout(() => {
+                let result = generator.next();
+                message = result.value;
+                if (!result.done && counter < 1000) runTest();
+            }, 100);
+        };
+        runTest();
+    });
+};
 
-for (let divisor=2; divisor<=divisorMax; divisor++) {
-    const success = test(trainingSize, testSize, trainingCycles, (number) => (number%divisor===0?1:0));
-    console.log(`Finding multiples of ${divisor}: Success ${success}% of the time.`);
+function* run() {
+    const divisorMax = getValue('divisorMax', 10);
+    const trainingSize = getValue('trainingSize',300);
+    const testSize = getValue('testSize',50);
+    const trainingCycles = getValue('trainingCycles',200);
+
+    yield `Training Size: ${trainingSize}, TestSize: ${testSize}, Training Cycles: ${trainingCycles}`;
+
+    for (let divisor=2; divisor<=divisorMax; divisor++) {
+        const success = test(trainingSize, testSize, trainingCycles, (number) => (number%divisor===0?1:0));
+        yield `Finding multiples of ${divisor}: Success ${success}% of the time.`;
+    }
+
+    let success = test(trainingSize, trainingSize, trainingCycles,(number) => isPrime(number));
+    yield `Finding Prime: Success ${success}% of the time.`;
+
+    success = test(trainingSize, trainingSize, trainingCycles,(number) => isSquare(number));
+    yield `Finding Square: Success ${success}% of the time.`;
+
+    success = test(trainingSize, trainingSize, trainingCycles,(number) => isFibonacci(number));
+    yield `Finding Fibonacci: Success ${success}% of the time.`;
+
 }
 
-let success = test(trainingSize, trainingSize, trainingCycles,(number) => isPrime(number));
-console.log(`Finding Prime: Success ${success}% of the time.`);
+function clear() {
+    const div = document.getElementById('results');
+    let ul = div.getElementsByTagName('ul')[0];
+    div.removeChild(ul);
+    ul = document.createElement('ul');
+    div.appendChild(ul);
+}
 
-success = test(trainingSize, trainingSize, trainingCycles,(number) => isSquare(number));
-console.log(`Finding Square: Success ${success}% of the time.`);
-
-success = test(trainingSize, trainingSize, trainingCycles,(number) => isFibonacci(number));
-console.log(`Finding Fibonacci: Success ${success}% of the time.`);
+function getValue(elementId, defaultValue) {
+    const el = document.getElementById(elementId);
+    if (el === null) return defaultValue;
+    const value = el.value;
+    return (value === undefined || value === null ? defaultValue : parseInt(value));
+}
